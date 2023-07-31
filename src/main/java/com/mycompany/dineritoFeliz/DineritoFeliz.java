4fdaea47a993048dd2fc9366adff578281fbd73f
cristianIvanGitHub <139143081+cristianIvanGitHub@users.noreply.github.com>
2023-07-30 19:28:18 -0600

    Inicio del proyecto de un gestor de ventas de una tienda Se crearon las interfaces graficas del muenu, como del la ventana de ingresar datos

---
+++ src/main/java/com/mycompany/dineritoFeliz/DineritoFeliz.java
@@ -0,0 +1,19 @@
+
+
+package com.mycompany.dineritoFeliz;
+
+import com.mycompany.dineritoFeliz.igu.Menu;
+/**
+ *
+ * @author malaf
+ */
+public class DineritoFeliz {
+
+    public static void main(String[] args) {
+         Menu pantalla = new Menu();
+         
+         pantalla.setVisible(true);
+         pantalla.setLocationRelativeTo(null);
+        
+    }
+}
